bfb3d88b8e5761349827d074095b7a421d80f0a6
Мария <1481047@mail.ru>
2021-12-07 22:45:22 +0300

    Добавьте файлы проекта.

---
+++ VS16.1.1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace VS16._1._1
+{
+    class Program
+    {
+        //Сериализовать массив в json-строку, сохранить ее программно в файл «Products.json».
+        static void Main(string[] args)
+        {
+            Product[] product = new Product[5];
+            product[0] = new Product();
+            product[1] = new Product();
+            product[2] = new Product();
+            product[3] = new Product();
+            product[4] = new Product();
+            foreach (var i in product)
+            {
+                i.DataInput();
+            }
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            string jsonString = JsonSerializer.Serialize(product, options);
+            File.WriteAllText("Java/Products.json", jsonString);            
+            Console.ReadKey();
+        }
+    }
+    class Product
+    {
+        public int codeProduct { get; set; }
+        public string nameProduct { get; set; }
+        public double priceProduct { get; set; }
+        public void DataInput()
+        {
+            Console.WriteLine("Товар #");
+            Console.WriteLine("Введите «Код товара»");
+            codeProduct = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите «Название товара»");
+            nameProduct = Console.ReadLine();
+            Console.WriteLine("Введите  «Цена товара»");
+            priceProduct = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
---
+++ VS16.1.2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+
+
+namespace VS16._1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Десериализовать файл «Products.json» из задачи 1. Определить название самого дорогого товара.
+            try
+            {
+                string jsonString = File.ReadAllText("D:/Visual/VS16.1.1/VS16.1.1/bin/Debug/Java/Products.json");
+                Product[] product = JsonSerializer.Deserialize<Product[]>(jsonString);
+
+                foreach (var item in product)
+                {
+                    item.DataInput();
+                }
+                Product[] product1 = JsonSerializer.Deserialize<Product[]>(jsonString);
+                double max = 0;
+                string maxName = "";
+                foreach (var i in product1)
+                {
+                    if (i.priceProduct > max)
+                    {
+                        max = i.priceProduct;
+                        maxName = i.nameProduct;
+                    }
+                }
+                Console.WriteLine("\nСамый дорогой товар это - {0}", maxName);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+    class Product
+
+    {
+        public int codeProduct { get; set; }
+        public string nameProduct { get; set; }
+        public double priceProduct { get; set; }
+        public void DataInput()
+
+        {
+            Console.WriteLine("\nТовар #");
+            Console.WriteLine("\n{0}", codeProduct);
+            Console.WriteLine(nameProduct);
+            Console.WriteLine(priceProduct);
+        }
+
+    }
+}
+
+
